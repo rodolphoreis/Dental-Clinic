e96f52cf0ca4093b632e85bcfa8a070461a17ec2
Rodolpho Reis <rodolphoreis@live.com>
2024-01-30 17:42:09 +0000

    feat: integrate Formspree for form submission

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,17 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import "./ContactForm.css";
+
 function ContactForm() {
   const [state, handleSubmit] = useForm("xgegoowj");
+  const [formData, setFormData] = useState({
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e && e.preventDefault();
+    e.preventDefault();
+
+    try {
+      const response = await fetch("https://formspree.io/f/xgegoowj", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setFormData({
+          email: "",
+          message: "",
+        });
+      } else {
+        console.error("Erro ao enviar formulário:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Erro durante a submissão do formulário:", error);
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleFormSubmit}>
       <h2>Agende uma Consulta</h2>
       <label htmlFor="email">Email</label>
-      <input id="email" type="email" name="email" />
+      <input
+        id="email"
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+      />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       <label htmlFor="message">Mensagem</label>
-      <textarea id="message" name="message" />
+      <textarea
+        id="message"
+        name="message"
+        value={formData.message}
+        onChange={handleChange}
+      />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
 
       {state.submitting ? (
